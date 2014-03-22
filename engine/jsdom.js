18760f6159f4aae8932e5bacf5041e17a686c322
suisho <suisho000000@gmail.com>
2014-03-22 10:55:41 +0900

    split compute,js

--- engine/jsdom.js
+++ engine/jsdom.js
@@ -1,18 +1,8 @@
 var jsdom = require('jsdom')
-
-function computeStyle(window, selectors){
-  var document = window.document
-
-  var result = {}
-  selectors.forEach(function(selector){
-    var elm = document.querySelector(selector)
-    result[selector] = window.getComputedStyle(elm).cssText
-  })
-  return result
-}
+var computeStyles = require("../lib/compute.js")
 
 module.exports = function(html, selectors, cb){
   jsdom.env(html, function(err, window){
-    cb(null, computeStyle(window, selectors))
+    cb(null, computeStyles(window, selectors))
   })
 }
