5e137510467dfe38bcee9c43a7401c6d97532f4e
suisho <suisho000000@gmail.com>
2014-03-23 22:52:40 +0900

    fix runner call

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@ var nativeEngine = require('./engine/native')
 var runner = require('./lib/runner')
 
 window.gcs = module.exports = function(selectors, cb){
-  runner(selectors, nativeEngine, function(err, result){
-    cb(err, reuslt)
+  runner(null, selectors, nativeEngine, function(err, result){
+    cb(err, result)
   })
 }
