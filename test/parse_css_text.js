f74b09c1a89608f99d5ef5912387319fffa8b7fb
suisho <suisho000000@gmail.com>
2014-03-23 22:53:33 +0900

    split csstext

--- test/parse_css_text.js
+++
@@ -1,9 +0,0 @@
-var csstext = require("../lib/csstext")
-var assert = require("assert")
-it("parse", function(){
-  var text = 'color: red; background-color: blue;'
-  assert(csstext(text), {
-    "color": "red",
-    "background-color" : "blue"
-  })
-})
