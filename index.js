abec95380997380244048f774888050641022720
suisho <suisho000000@gmail.com>
2014-03-27 09:38:01 +0900

    remove runner

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var jsdomEngine = require('./engine/jsdom')
+var cheerioEngine = require('./engine/cheerio')
 var runner = require('./lib/runner')
 
-
+//var engine = cheerioEngine
+var engine = jsdomEngine
 module.exports = function(html, selectors, cb){
-  runner(html, selectors, jsdomEngine, function(err, result){
+  engine(html, selectors, function(err, result){
     cb(err, result)
   })
 }
