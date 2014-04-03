112ebbc47eedb1dbe0624aa329effd76891bf6d7
suisho <suisho000000@gmail.com>
2014-04-04 00:15:47 +0900

    remove

--- browser.js
+++
@@ -1,8 +0,0 @@
-var nativeEngine = require('./engine/native')
-var runner = require('./lib/runner')
-
-window.gcs = module.exports = function(selectors, cb){
-  runner(null, selectors, nativeEngine, function(err, result){
-    cb(err, result)
-  })
-}
