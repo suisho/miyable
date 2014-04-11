e7600a68728be1c9d46660add14bc566e385a20c
suisho <suisho000000@gmail.com>
2014-04-12 00:48:51 +0900

    wip

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var jsdomEngine = require('./engine/jsdom')
 var cheerioEngine = require('./engine/cheerio')
 
 var engine = cheerioEngine
-//var engine = jsdomEngine
-module.exports = function(html, selectors, cb){
-  engine(html, selectors, function(err, result){
+
+module.exports = function(selectors, cb){
+  engine(selectors, function(err, result){
     cb(err, result)
   })
 }
